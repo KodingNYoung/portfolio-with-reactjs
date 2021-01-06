e1b8fea02d7b4bd818242018229ca484dca7506f
KodingNYoung <abiodunadebambo44@gmail.com>
2021-01-06 05:33:44 +0100

    updated the social media handles

--- src/Components/socialHandles/SocialHandles.js
+++ src/Components/socialHandles/SocialHandles.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGithub,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
-import './socialHandles.css'
+import "./socialHandles.css";
 
 const SocialHandles = ({ color }) => {
   const linkStyle = {
-    color: color
-  }
+    color: color,
+  };
   return (
     <div className="socialMediaHandles">
-      <a href="https://twitter.com/18_youngcrown" style={linkStyle} target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faTwitter}/>
+      <a
+        href="https://twitter.com/kodingnyoung"
+        style={linkStyle}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter} />
       </a>
-      <a href="https://github.com/KodingNYoung/" style={linkStyle} target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faGithub}/>
+      <a
+        href="https://github.com/KodingNYoung/"
+        style={linkStyle}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} />
       </a>
-      <a href="https://www.linkedin.com/in/abiodun-adebambo-5316551a6/" style={linkStyle} target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faLinkedinIn}/>
+      <a
+        href="https://www.linkedin.com/in/abiodun-adebambo/"
+        style={linkStyle}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedinIn} />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default SocialHandles
+export default SocialHandles;
