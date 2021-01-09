ced933b303f53b9d504f0e8ab4c1fc324ad16672
KodingNYoung <abiodunadebambo44@gmail.com>
2021-01-09 17:08:39 +0100

    get data from firebase

--- src/views/About.js
+++ src/views/About.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import Heading from '../Components/Heading/Heading';
+import React from "react";
+import Heading from "../Components/Heading/Heading";
 
-const About = () => {
+const About = ({ about }) => {
   return (
     <main className="about">
-      <Heading title="a little about me."/>
-      <article>
-        <p>
-          I'm <span>Adebambo Abiodun Oluwasegun</span> but you can call me KodingNYoung. I'm a self-taught, frontend web developer.
-        </p>
-        <p>
-          I started my coding journey in 2018 and since then have built different websites and web apps with my knowledge of HTML, CSS, Javascript and other frontend technologies like <span>Bootstrap</span>, <span>Material design</span> and <span>Reactjs</span>. 
-        </p>
-        <p>
-          Working with different teams over the years has helped build my <span>teamwork</span> and improve my knowledge of <span>git and github workflow</span>.
-          
-        </p>
-        <p>
-          In my leisure time I play with Arduino, mentor younger devs, watch movies, listen to music, and watch my favourite football club.
-        </p>
-      </article>
+      <Heading title="a little about me." />
+      <article
+        dangerouslySetInnerHTML={{
+          __html: about,
+        }}
+      />
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
