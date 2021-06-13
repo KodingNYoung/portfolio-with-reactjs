aada43f048443f00f209ab9f8334f3fb67b74576
KodingNYoung <abiodunadebambo44@gmail.com>
2021-06-13 01:07:23 +0100

    fixed the image stretch on the project page and error om the preloader

--- src/Components/Cards/ProjectCard.js
+++ src/Components/Cards/ProjectCard.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExternalLinkAlt, faCode } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt, faCode } from "@fortawesome/free-solid-svg-icons";
 
-import './ProjectCard.css'
+import "./ProjectCard.css";
 
 const ProjectCard = ({ project }) => {
-  const { url, project: {title, description, githubRepoLink, liveWebsiteLink, stacks}} = project;
+  const {
+    url,
+    project: { title, description, githubRepoLink, liveWebsiteLink, stacks },
+  } = project;
   return (
     <div className="project-card">
       <div className="cardImg-container">
-        <img src={url} alt="card"/>
+        <img src={url} alt="card" />
       </div>
       <div className="overlay-info">
-        <h2 className="title">{title}.</h2>
-        <p className="stacks">
-          {stacks}
-        </p>
+        <header>
+          <h2 className="title">{title}.</h2>
+          <p className="stacks">{stacks}</p>
+        </header>
         <p className="description">{description}</p>
         <footer>
-          <a href={liveWebsiteLink} title="Live website" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faExternalLinkAlt}/>
+          <a
+            href={liveWebsiteLink}
+            title="Live website"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
           </a>
-          <a href={githubRepoLink} title="Github repo" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faCode}/>
+          <a
+            href={githubRepoLink}
+            title="Github repo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faCode} />
           </a>
         </footer>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProjectCard;
--- src/views/Project.js
+++ src/views/Project.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import Heading from '../Components/Heading/Heading';
-import Preloader from '../Components/Preloader/Preloader';
-import ProjectCard from '../Components/Cards/ProjectCard';
-import useFirestore from '../firebase/hooks/useFirestore'
+import React, { useState, useEffect } from "react";
+import Heading from "../Components/Heading/Heading";
+import Preloader from "../Components/Preloader/Preloader";
+import ProjectCard from "../Components/Cards/ProjectCard";
+import useFirestore from "../firebase/hooks/useFirestore";
 
 const ProjectCards = () => {
-  const { docs } = useFirestore('projects');
+  const { docs } = useFirestore("projects");
   const [showPreloader, setShowPreloader] = useState(true);
   useEffect(() => {
-    if(docs[0]) {
+    if (docs[0]) {
       setShowPreloader(false);
     }
-  }, [docs])
-  console.log(docs)
+  }, [docs]);
+  console.log(docs);
   return (
     <div className="project-cards-list">
       {showPreloader && (
-        <Preloader messsage="Loadingprojects... Make sure your internet connection is turned on." />
+        <Preloader messsage="Loading projects... Make sure your internet connection is turned on." />
       )}
       {docs.map((doc) => {
         return <ProjectCard key={doc.id} project={doc} />;
       })}
     </div>
   );
-}
+};
 
 const Project = () => {
   return (
@@ -31,7 +31,7 @@ const Project = () => {
       <Heading title="showcase section." />
       <ProjectCards />
     </section>
-  )
-}
+  );
+};
 
 export default Project;
